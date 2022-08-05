e953ca1bd43d834899ede2ad9bd3fa6cc91c12cb
Светлана <svetlanagreenfox@gmail.com>
2022-08-05 22:29:25 +0300

    Create the project

---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/order */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+// eslint-disable-next-line import/no-extraneous-dependencies, quotes
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+});
